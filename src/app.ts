9233cb609c5c5a8f2841b1be9ae4b3a1b754d90f
thverse <wowo1127@naver.com>
2021-03-19 16:53:53 +0900

    middleware 등록

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,8 @@
 import express, { Application } from 'express';
 import morgan from 'morgan';
 import IndexRoutes from './routes/indexRoutes';
-
+import expressSession from 'express-session';
+import cookieParser from 'cookie-parser';
 
 
 export class App {
@@ -17,11 +18,22 @@ export class App {
 
     private settings(){
          this.app.set('port', this.port || process.env.PORT || 1127);
+         
     }
 
     private middlewares(){
         this.app.use(morgan('dev')); 
         this.app.use(express.json());
+        this.app.use(cookieParser());
+        this.app.use(expressSession({
+            secret: 'my server key',       
+            resave: true,
+            saveUninitialized:true
+        }));
+         
+        
+        
+        출처: https://3dmpengines.tistory.com/1870 [3DMP]
         
     }
 
